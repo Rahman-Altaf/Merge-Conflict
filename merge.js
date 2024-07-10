6a07064497be690bfb742784fa9d3dae41e2dc4e
Rahman-Altaf <rahmanch.311@gmail.com>
2024-07-11 01:57:18 +0500

    evenodd--add func

--- merge.js
+++ merge.js
@@ -1,18 +1,6 @@
-
-function addNumbers1(a, b) {
-    return a + b;
+const evenOdd = ()=> {
+    console.log("hello 1");
 }
-
-function multiplyNumbers1(a, b) {
-    return a * b;
+const add = ()=> {
+    console.log("hello");
 }
-
-
-const num1 = 5;
-const num2 = 10;
-
-const sum = addNumbers(num1, num2);
-const product = multiplyNumbers(num1, num2);
-
-console.log(`The sum of ${num1} and ${num2} is: ${sum}`);
-console.log(`The product of ${num1} and ${num2} is: ${product}`);
