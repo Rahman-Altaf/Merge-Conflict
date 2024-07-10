7a6e2d370067d383022b03d37efb030904c4ebca
Rahman-Altaf <rahmanch.311@gmail.com>
2024-07-11 01:58:38 +0500

    abdulrehman--add func

--- merge.js
+++ merge.js
@@ -1,18 +1,6 @@
-
-function addNumbers2(a, b) {
-    return a + b;
+const abdulRehman = ()=> {
+    console.log("hello 2");
 }
-
-function multiplyNumbers2(a, b) {
-    return a * b;
+const add = ()=> {
+    console.log("hello");
 }
-
-
-const num1 = 5;
-const num2 = 10;
-
-const sum = addNumbers(num1, num2);
-const product = multiplyNumbers(num1, num2);
-
-console.log(`The sum of ${num1} and ${num2} is: ${sum}`);
-console.log(`The product of ${num1} and ${num2} is: ${product}`);
