8aef9d01ac88fb7ac8ba693552428ceb0bc42e35
Rahman-Altaf <rahmanch.311@gmail.com>
2024-07-11 01:45:30 +0500

    New Code Embed

--- merge.js
+++ merge.js
@@ -1,3 +1,18 @@
-const merge2 =()=>{
-    console.log("1 Hello World Merge Conflict")
-} 
+
+function addNumbers(a, b) {
+    return a + b;
+}
+
+function multiplyNumbers(a, b) {
+    return a * b;
+}
+
+
+const num1 = 5;
+const num2 = 10;
+
+const sum = addNumbers(num1, num2);
+const product = multiplyNumbers(num1, num2);
+
+console.log(`The sum of ${num1} and ${num2} is: ${sum}`);
+console.log(`The product of ${num1} and ${num2} is: ${product}`);
